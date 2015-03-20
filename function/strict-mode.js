448ee8a1fdecc15890f3ee376050e93b6488246a
Elf-mousE <ifmouz@qq.com>
2015-03-20 16:24:10 +0800

    es5

--- function/strict-mode.js
+++ function/strict-mode.js
@@ -1,1 +1,20 @@
 'use strict';
+
+/**
+ * Strict mode for scripts
+ */
+
+// Whole-script strict mode syntax
+"use strict";
+var v = "Hi!  I'm a strict mode script!";
+
+/**
+ * Strict mode for functions
+ */
+function strict(){
+  // Function-level strict mode syntax
+  'use strict';
+  function nested() { return "And so am I!"; }
+  return "Hi!  I'm a strict mode function!  " + nested();
+}
+function notStrict() { return "I'm not strict."; }
